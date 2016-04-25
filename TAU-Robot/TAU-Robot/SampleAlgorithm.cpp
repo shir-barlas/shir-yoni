552eb1286f0a3a50b517dfd0b6dd13126eab1b9e
shir barlas <shirbarlas@gmail.com>
2016-04-25 20:16:48 +0300

    simple random algorithm

---
+++ TAU-Robot/TAU-Robot/SampleAlgorithm.cpp
@@ -0,0 +1,48 @@
+#include "AbstractAlgorithm.h"
+#include "Direction.h"
+#include <cstdlib>
+
+class SampleAlgorithm : AbstractAlgorithm
+{
+	int maxStepsAfrterWinner;
+	int batteryCapacity;
+	int batteryConsumptionRate;
+	int batteryRechargeRate;
+	int stepsToFinish;
+	const AbstractSensor *algorithmSensor;
+
+public:
+	void setSensor(const AbstractSensor& sensor)
+	{
+		algorithmSensor = &sensor;
+	}
+
+	void setConfiguration(map<string, int> config)
+	{
+		maxStepsAfrterWinner = config["maxStepsAfterWinner"];
+		batteryCapacity = config["batteryCapacity"];
+		batteryConsumptionRate = config["batteryConsumptionRate"];
+		batteryRechargeRate = config["batteryRechargeRate"];
+		stepsToFinish = INFINITY;
+	}
+
+	// step is called by the simulation for each time unit 
+	Direction step()
+	{
+		while (true)
+		{
+			int randomStep = rand() % 5;
+			SensorInformation info = algorithmSensor->sense();
+			if (!info.isWall[randomStep])
+				return (Direction)randomStep;
+		}
+	}
+
+	// this method is called by the simulation either when there is a winner or 
+	// when steps == MaxSteps - MaxStepsAfterWinner 
+	// parameter stepsTillFinishing == MaxStepsAfterWinner 
+	void aboutToFinish(int stepsTillFinishing) 
+	{
+		stepsToFinish = stepsTillFinishing;
+	}
+};
